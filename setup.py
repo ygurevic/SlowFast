97aaf4d79b995845c19f28406ed1bae307517279
Yehonatan Gurevich <108222280+ygurevic@users.noreply.github.com>
2022-08-09 15:45:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "opencv-python",
         "pandas",
         "torchvision>=0.4.2",
-        "PIL",
+        "pillow",
         "sklearn",
         "tensorboard",
         "fairscale",
